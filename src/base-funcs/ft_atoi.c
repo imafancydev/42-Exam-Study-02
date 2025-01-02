b095a87f981426ebf72880ff3fb14c1bb0dec18a
Lucas Leão <lucasaugustoleon@gmail.com>
2025-01-01 22:30:56 -0300

    atoi

--- src/base-funcs/ft_atoi.c
+++ src/base-funcs/ft_atoi.c
@@ -6,7 +6,9 @@
 /*   By: laugusto <laugusto@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/30 14:03:09 by laugusto          #+#    #+#             */
-/*   Updated: 2024/12/30 14:03:10 by laugusto         ###   ########.fr       */
+/*   Updated: 2025/01/01 22:30:35 by laugusto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+int ft_atoi(const char *nptr);

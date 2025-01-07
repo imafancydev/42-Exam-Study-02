f9e3e70029253c0a7de4a90f23e5db3085e73c54
Lucas Leão <lucasaugustoleon@gmail.com>
2025-01-06 23:38:34 -0300

    str_capitalizer

--- Characters (original order)/str_capitalizer.c
+++ Characters (original order)/str_capitalizer.c
@@ -6,28 +6,48 @@
 /*   By: laugusto <laugusto@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 15:48:16 by laugusto          #+#    #+#             */
-/*   Updated: 2025/01/06 16:06:29 by laugusto         ###   ########.fr       */
+/*   Updated: 2025/01/06 23:38:20 by laugusto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int main(int gc, char **gv)
+int	is_space(char c)
 {
-    if(gc != 2)
-    {
-        write(1, "\n", 1);
-        return (0);       
-    }
+	if (c <= 32)
+		return (1);
+	return (0);
+}
 
-    int i;
+void	str_capitalazer(char *str)
+{
+	int	i;
 
-    while(gv[1][i])
-    {
-        while(gv[1][i] != ' '){
-            if(gv[1][i] >= 'a' && gv[1][i] >= 'z')
-                gv[1][i] =- 32;
-            i++;
-        }
-    }
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		if ((str[i] >= 'a' && str[i] <= 'z') && is_space(str[i - 1]))
+			str[i] -= 32;
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	main(int gc, char **gv)
+{
+	if (gc == 1)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+
+	int i = 1;
+	while (i < gc)
+	{
+		str_capitalazer(gv[i]);
+		write(1, "\n", 1);
+		i++;
+	}
 }
